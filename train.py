5dbeadcfff45597065c3fae23cc030991e554209
VisionAIGeek99 <kmw2622@naver.com>
2025-06-02 15:34:15 +0900

    feat: train.py add

---
+++ train.py
@@ -0,0 +1,31 @@
+import torch
+from torch.utils.data import DataLoader
+
+import hydra
+from hydra.utils import instantiate
+from omegaconf import DictConfig
+
+
+@hydra.main(config_path="conf", config_name="config", version_base="1.1")
+def main(cfg: DictConfig):
+    txt_path = cfg.dataset_meta.raw_path
+    context_length = cfg.dataset_meta.context_length
+    batch_size = cfg.training.batch_size
+    tokenizer = instantiate(cfg.tokenizer, text=txt_path)
+    tokenizer.load(cfg.vocab.vocab_path)
+
+    model = instantiate(cfg.model)
+    dataset = instantiate(cfg.dataset, path=txt_path, tokenizer=tokenizer, context_length=context_length)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=cfg.training.shuffle)
+
+    for x, y in dataloader:
+        x_decoded = tokenizer.decode(x[0].tolist())
+        y_decoded = tokenizer.decode(y[0].tolist())
+        print(f"x_decoded: {x_decoded}")
+        print(f"y_decoded: {y_decoded}")
+
+        break
+
+
+if __name__ == "__main__":
+    main()
