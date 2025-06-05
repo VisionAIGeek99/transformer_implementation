32b2f49181630be85479bd0d9c7ccb38317d1e51
VisionAIGeek99 <kmw2622@naver.com>
2025-06-05 17:19:50 +0900

    feat: scaled_dot_product_attention full implementation add

--- models/transformer/layers/scaled_dot_product_attention.py
+++ models/transformer/layers/scaled_dot_product_attention.py
@@ -1,13 +1,27 @@
-import torch
+import math
 from torch import nn
 
 class ScaledDotProductAttention(nn.Module):
     def __init__(self):
         super().__init__()
-        self.softmax = nn.Softmax(dim=-1)
+        self.softmax = nn.Softmax(dim=-1) # apply for dim of key  
 
 
     def forward(self, q, k, v, mask=None):
+        batch_size, head, length, d_tensor = k.size()
+        
+        # 1. dot product Query with Key^T to compute similarity
+        k_t = k.transpose(2, 3)  # transpose
+        score = (q @ k_t) / math.sqrt(d_tensor)  # scaled dot product
 
-        k_t = k.transpose
-        pass
+        # 2. apply masking (opt)
+        if mask is not None:
+            score = score.masked_fill(mask == 0, -10000)
+
+        # 3. pass them softmax to make [0, 1] range
+        score = self.softmax(score)
+
+        # 4. multiply with Value
+        v = score @ v
+
+        return v, score
