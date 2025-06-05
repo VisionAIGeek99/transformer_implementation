72622fcfbec51c8cd7ee0679ebbca695d000fff7
VisionAIGeek99 <kmw2622@naver.com>
2025-06-05 17:39:24 +0900

    feat: position_wise_feed_forward.py add

---
+++ models/transformer/layers/position_wise_feed_forward.py
@@ -0,0 +1,16 @@
+from torch import nn
+
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_hidden, drop_prob=0.1):
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_hidden)
+        self.linear2 = nn.Linear(d_hidden, d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_prob)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
